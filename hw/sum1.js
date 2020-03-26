7fa2ba36a24e4b2a8cae1fc940f5250d045c042c
Kim HeeJae <iowa329@gmail.com>
2020-03-26 18:16:00 +0900

    [Unstage] sum*.js 과제 삭제

--- hw/sum1.js
+++
@@ -1,6 +0,0 @@
-let sum = 0
-for(num = 1; num <= 55; num++) {
-	sum += num
-}
-
-console.log(sum)
--- hw/sum2.js
+++
@@ -1,9 +0,0 @@
-let sum = 0
-let num = 0
-for(num = 1; num <= 55; num++) {
-	if (num%3 == 0) {
-		sum += num
-	}
-}
-
-console.log(sum)
